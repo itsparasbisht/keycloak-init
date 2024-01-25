31a9c403248bb6f9abab49c058ae426c18eb726a
itsparasbisht <iamparasbisht@gmail.com>
2024-01-25 20:27:25 +0530

    setup rendered

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,12 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useState } from "react";
 import Keycloak from "keycloak-js";
 const envVars = import.meta.env;
 
 export default function useAuth() {
+  const isRendered = useRef(false);
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
+    if (isRendered.current) return;
+
+    isRendered.current = true;
+
     const client = new Keycloak({
       url: envVars.VITE_KEYCLOAK_URL,
       realm: envVars.VITE_KEYCLOAK_REALM,
@@ -17,7 +22,7 @@ export default function useAuth() {
       .init({
         onLoad: "login-required",
       })
-      .then((res) => console.log(res));
+      .then((res) => setIsLoggedIn(res));
   }, []);
 
   return isLoggedIn;
